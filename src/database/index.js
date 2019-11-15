c81bf81ac5de485cabedd3120605fcbb762e3160
guaracyalima <guaracyaraujolima@gmail.com>
2019-11-15 10:17:24 -0200

    add access database settings to initialize sequelizeORM

---
+++ src/database/index.js
@@ -0,0 +1,20 @@
+import Sequelize from 'sequelize';
+import databaseConfig from '../config/database';
+
+const models = [];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    // inicializa o sequelize com as credencias do db
+    this.connection = new Sequelize(databaseConfig);
+
+    // passa para cada model a conexao
+    models.map((model) => model.init(this.connection));
+  }
+}
+
+export default new Database();
